dfe0811991eedbc924dff4de0c1800d2e2af6dae
Ryada <rodericks@live.nl>
2019-06-04 23:09:29 +0200

    more wip

--- FluentDesign/Controls/FluentWindow.cs
+++ FluentDesign/Controls/FluentWindow.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Interop;
+﻿using System.Windows;
 using FluentDesign.Extensions;
 
 namespace FluentDesign.Controls
